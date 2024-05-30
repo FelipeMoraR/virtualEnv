8c0b89bfc5e3ea30db018f55882c89fa00299722
Felipe Mora <gnar2342@gmail.com>
2024-05-30 00:31:07 -0400

    [ADD] avance creacion de excel, agregar filas, fila defaul al crear un excel.

--- googleSheet/googleSheet.py
+++ googleSheet/googleSheet.py
@@ -15,33 +15,80 @@ cliente = gspread.authorize(credenciales) #Damos autorizacion para poder acceder
 # Crear un servicio de Google Drive
 drive_service = build('drive', 'v3', credentials=credenciales)
 
+def agregarFilasDefault(excel_id):
+    rows_to_add = [
+        ['Nombre Gasto', 'Valor']
+    ]
+    sheet = cliente.open_by_key(excel_id)
+    
+    worksheet = sheet.get_worksheet(0)
+    
+    worksheet.append_rows(rows_to_add)
+    print(f"{len(rows_to_add)} filas agregadas en la hoja de cálculo {excel_id}.")
+
+
+def agregarNuevasFilas(excel_id, hoja_trabajo_nombre, filas):
+    # Abrir la hoja de cálculo por ID
+    sheet = cliente.open_by_key(excel_id)
+    # Seleccionar la hoja de trabajo por nombre, por ahora solo agrega en la primera hoja de trabajo
+    worksheet = sheet.get_worksheet(0)
+    # Agregar filas una debajo de otra
+    worksheet.append_rows(filas)
+    print(f"{len(filas)} filas agregadas en la hoja de cálculo {excel_id}.")
+
+def obtenerSheet(nombreExcel):
+    query = f"name = '{nombreExcel}' and mimeType = 'application/vnd.google-apps.spreadsheet'" # mimeType es simplemente para definir que tipo de documento/archivo es, puedes poner imagenes, audios, words, etc.
+    results = drive_service.files().list(q=query, fields="files(id, name)").execute()
+    files = results.get('files', []) #Agarra el archivo y lo guarda en un array
+
+    if files:
+        objetoExcel = files[0]
+        return objetoExcel
+    else:
+        return False
+
 def verificarExistenciaSheet(nombreExcel):
     query = f"name = '{nombreExcel}' and mimeType = 'application/vnd.google-apps.spreadsheet'" # mimeType es simplemente para definir que tipo de documento/archivo es, puedes poner imagenes, audios, words, etc.
     results = drive_service.files().list(q=query, fields="files(id, name)").execute()
     files = results.get('files', [])
     
-    if files:
-        print(files)
+    if files:    
         return True
     else:
         return False
 
-def compartiExcel(hojaCalculo, correo):
-    hojaCalculo.share(correo, perm_type = 'user', role = 'writer') 
+def compartiExcel(hojaCalculo):
+    hojaCalculo.share('felipestorage2@gmail.com', perm_type = 'user', role = 'writer') 
     return
 
-def crearExcel(nombreExcel, correo):
+def crearExcel(nombreExcel):
     if verificarExistenciaSheet(nombreExcel):
         print('El excel ya existe, porfavor ingresa otro nombre')
         return False
     else:
         print('Creando excel')
-        sheet = cliente.create(nombreExcel, correo)
+        sheet = cliente.create(nombreExcel)
         compartiExcel(sheet)
+        nuevoExcel = obtenerSheet(nombreExcel)
+        agregarFilasDefault(nuevoExcel['id'])
+
         return True
     
 
 # Verificar si existe una hoja de cálculo con un nombre específico
-nombre_hoja = "testNuevo"
-correo = 'felipestorage2@gmail.com'
-crearExcel(nombre_hoja, correo)
+nombre_hoja = "algonuevoExcelGood2"
+
+
+rows_to_add = [
+    ['Nombre', 'Apellido', 'Edad'],
+    ['Juan', 'Perez', '30'],
+    ['Ana', 'Gomez', '25']
+]
+
+#if obtenerSheet(nombre_hoja):
+    #objExcelEncontrado = obtenerSheet(nombre_hoja)
+    #agregarNuevasFilas(objExcelEncontrado['id'], objExcelEncontrado['name'], rows_to_add)
+    
+#else:
+    #print('no pasa nah')
+crearExcel(nombre_hoja)
