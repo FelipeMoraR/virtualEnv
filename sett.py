451291a7f6ab2f566f3a6007eed94810e3974b5d
Felipe Mora <gnar2342@gmail.com>
2024-06-14 21:56:41 -0400

    [test] test

--- sett.py
+++ sett.py
@@ -8,8 +8,6 @@ wspToken = 'EAAEOme1CvT8BO6SGqVjgYEZA9fLEtwoPDA34A8NUKZB9se6x4F3L53KACaNy4ZCwIL2
 wspUrl = 'https://graph.facebook.com/v19.0/331286213394604/messages'
 
 
-documentUrl = 'https://repository.javeriana.edu.co/bitstream/handle/10554/61464/attachment_1__pdf-prueba.pdf?sequence=2&isAllowed=y'
-
 
 credencialesJson = {
     "type": config('GOOGLE_TYPE'),
